ef9aa15f410160b939c9a7f00f45019fcf511c6d
Austerlitz-0 <1477567612@qq.com>
2023-01-08 20:06:12 +0800

    进度条增加颜色

--- proc/proc.c
+++ proc/proc.c
@@ -8,11 +8,12 @@
 void process()
 {
   char tmp[SIZE];
+  char wait[5]="|/-\\";
   memset(tmp,'\0',sizeof(tmp));
   int i=0;
   for(;i<=100;i++)
   {
-    printf("[%-100s][%d%%]\r",tmp,i);
+    printf("[\033[0;32;31m%-100s\033[m][%d%%][%c]\r",tmp,i,wait[i%4]);
     tmp[i]=STYLE;
     if(i!=99) tmp[i+1]='>';
     fflush(stdout);
